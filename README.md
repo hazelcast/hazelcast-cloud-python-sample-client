3e373e349e413ddba8818e91abb87cadaff714db
kloia daas <daas@kloia.com>
2018-11-08 02:38:02 +0100

    added python client

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# hazelcast-cloud-python-sample-client
-Sample Python Client For Hazelcast Cloud Community
+# Sample Python Client For Hazelcast Cloud
+
+Instructions:
+
+1- Clone the project 
+
+2- In your cloud console, open the cluster that you want to connect. Click to the button `Configure Hazelcast Client`. 
+
+3- In `hazelcast_cloud.py` change the `YOUR_CLUSTER_NAME`, `YOUR_CLUSTER_PASSWORD` and `YOUR_CLUSTER_DISCOVERY_TOKEN` with the values given at your hazelcast cloud console.
+
+3- In the project folder run `pip install hazelcast-python-client` and `python hazelcast_cloud.py`
+
---
+++ hazelcast_cloud.py
@@ -0,0 +1,29 @@
+import hazelcast
+import logging
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+
+    config = hazelcast.ClientConfig()
+
+    # Set up group name and password for authentication
+    config.group_config.name = "YOUR_CLUSTER_NAME"
+    config.group_config.password = "YOUR_CLUSTER_PASSWORD"
+
+    # Enable SSL for encryption. Default CA certificates will be used.
+    config.network_config.ssl_config.enabled = True
+
+    # Enable Hazelcast.Cloud configuration and set the token of your cluster.
+    config.network_config.cloud_config.enabled = True
+    config.network_config.cloud_config.discovery_token = "YOUR_CLUSTER_DISCOVERY_TOKEN"
+
+    # Start a new Hazelcast client with this configuration.
+    client = hazelcast.HazelcastClient(config)
+
+    my_map = client.get_map("map-on-the-cloud")
+    my_map.put("key", "hazelcast.cloud")
+
+    print(my_map.get("key"))
+
+    client.shutdown()
