57a81889e6c51bb30f44ebd18e28395adde65270
Sergey Dreger <sergey.dreger@hazelcast.com>
2021-05-20 16:37:30 +0300

    HZC-521 - Regular printing of progress

--- client.py
+++ client.py
@@ -24,12 +24,16 @@ my_map.put("key", "value")
 if my_map.get("key") == "value":
     print("Successfully connected!")
     print("Now, 'map' will be filled with random entries.")
+
+    iteration_counter = 0
     while True:
         random_key = random.randint(1, 100000)
         random_key_str = str(random_key)
         my_map.put("key" + random_key_str, "value" + random_key_str)
         my_map.get("key" + random_key_str)
-        if random_key % 10 == 0:
+        iteration_counter += 1
+        if iteration_counter == 10:
+            iteration_counter = 0
             print("Map size:", my_map.size())
 else:
     client.shutdown()
