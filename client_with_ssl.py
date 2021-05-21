dc97dd00817d680a21fcf62f52205717cf004708
sdreger <sergey.dreger@gmail.com>
2021-05-21 14:18:19 +0300

    HZC-521 - Regular printing of progress (#4)
    * HZC-521 - Regular printing of progress
    
    * HZC-521 - Remove the redundant 'random_key_str' variable

--- client_with_ssl.py
+++ client_with_ssl.py
@@ -30,12 +30,15 @@ my_map.put("key", "value")
 if my_map.get("key") == "value":
     print("Successfully connected!")
     print("Now, 'map' will be filled with random entries.")
+
+    iteration_counter = 0
     while True:
-        random_key = random.randint(1, 100000)
-        random_key_str = str(random_key)
-        my_map.put("key" + random_key_str, "value" + random_key_str)
-        my_map.get("key" + random_key_str)
-        if random_key % 10 == 0:
+        random_key = str(random.randint(1, 100000))
+        my_map.put("key" + random_key, "value" + random_key)
+        my_map.get("key" + random_key)
+        iteration_counter += 1
+        if iteration_counter == 10:
+            iteration_counter = 0
             print("Map size:", my_map.size())
 else:
     client.shutdown()
