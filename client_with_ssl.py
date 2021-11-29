50b919802eb577a52fdf14cb24504357cec7e397
Sergey Dreger <sergey.dreger@hazelcast.com>
2021-11-29 17:24:54 +0200

    HZC-2052: Add SQL section to the 5.0 client sample

--- client_with_ssl.py
+++ client_with_ssl.py
@@ -11,6 +11,61 @@ After successful connection, it puts random entries into the map.
 See: https://docs.hazelcast.cloud/docs/python-client
 """
 
+
+def map_example(my_map):
+    print("Now, 'map' will be filled with random entries.")
+
+    iteration_counter = 0
+    while True:
+        random_key = str(random.randint(1, 100000))
+        my_map.put("key" + random_key, "value" + random_key)
+        my_map.get("key" + random_key)
+        iteration_counter += 1
+        if iteration_counter == 10:
+            iteration_counter = 0
+            print("Map size:", my_map.size())
+
+
+def sql_example(hz_client):
+    print("Creating a mapping...")
+    # See: https://docs.hazelcast.com/hazelcast/5.0/sql/mapping-to-maps
+    mapping_query = "CREATE MAPPING cities TYPE IMap OPTIONS ('keyFormat'='varchar','valueFormat'='varchar')"
+    hz_client.sql.execute(mapping_query)
+    print("The mapping has been created successfully.")
+    print("--------------------")
+
+    print("Inserting data via SQL...")
+    insert_query = """
+    INSERT INTO cities VALUES
+    ('Australia','Canberra'),
+    ('Croatia','Zagreb'),
+    ('Czech Republic','Prague'),
+    ('England','London'),
+    ('Turkey','Ankara'),
+    ('United States','Washington, DC');
+    """
+    hz_client.sql.execute(insert_query)
+    print("The data has been inserted successfully.")
+    print("--------------------")
+
+    print("Retrieving all the data via SQL...")
+    result = hz_client.sql.execute("SELECT * FROM cities").result()
+    for row in result:
+        country = row.get_object_with_index(0)
+        city = row.get_object_with_index(1)
+        print("%s - %s" % (country, city))
+    print("--------------------")
+
+    print("Retrieving a city name via SQL...")
+    result = hz_client.sql.execute("SELECT __key, this FROM cities WHERE __key = ?", "United States").result()
+    for row in result:
+        country = row.get_object("__key")
+        city = row.get_object("this")
+        print("Country name: %s; City name: %s" % (country, city))
+        print("--------------------")
+        exit(0)
+
+
 logging.basicConfig(level=logging.INFO)
 HazelcastCloudDiscovery._CLOUD_URL_BASE = "YOUR_DISCOVERY_URL"
 client = hazelcast.HazelcastClient(
@@ -24,22 +79,17 @@ client = hazelcast.HazelcastClient(
     ssl_password="YOUR_SSL_PASSWORD",
 )
 
-my_map = client.get_map("map").blocking()
-my_map.put("key", "value")
+sample_map = client.get_map("map").blocking()
+sample_map.put("key", "value")
 
-if my_map.get("key") == "value":
+if sample_map.get("key") == "value":
     print("Successfully connected!")
-    print("Now, 'map' will be filled with random entries.")
 
-    iteration_counter = 0
-    while True:
-        random_key = str(random.randint(1, 100000))
-        my_map.put("key" + random_key, "value" + random_key)
-        my_map.get("key" + random_key)
-        iteration_counter += 1
-        if iteration_counter == 10:
-            iteration_counter = 0
-            print("Map size:", my_map.size())
+    # the 'map_example' is an example with an infinite loop inside, so if you'd like to try other examples,
+    # don't forget to comment out the following line
+    map_example(sample_map)
+
+    # sql_example(client)
 else:
     client.shutdown()
     raise Exception("Connection failed, check your configuration.")
