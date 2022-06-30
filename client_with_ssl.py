763bca1f00c4a41ff0995419b91764668ca5553b
JakeSCahill <jakecahillgh@gmail.com>
2022-06-30 14:25:01 +0100

    Allow docs to access and display code samples

--- client_with_ssl.py
+++ client_with_ssl.py
@@ -5,24 +5,41 @@ from hazelcast.discovery import HazelcastCloudDiscovery
 from os.path import abspath
 
 """
-This is boilerplate application that configures client to connect Hazelcast Cloud cluster.
-After successful connection, it puts random entries into the map.
+This is a boilerplate client application that connects to your Hazelcast Viridian cluster.
+See: https://docs.hazelcast.com/cloud/get-started
 
-See: https://docs.hazelcast.cloud/docs/python-client
+Snippets of this code are included as examples in our documentation,
+using the tag:: comments.
 """
 
 logging.basicConfig(level=logging.INFO)
+# tag::env[]
+# Define which environment to use such as production, uat, or dev
 HazelcastCloudDiscovery._CLOUD_URL_BASE = "YOUR_DISCOVERY_URL"
+# end::env[]
+# Configure the client to connect to the cluster
+# tag::config[]
 client = hazelcast.HazelcastClient(
     cluster_name="YOUR_CLUSTER_NAME",
+    # The cluster discovery token is a unique token that maps to the current IP address of the cluster.
+	# Cluster IP addresses may change.
+	# This token allows clients to find out the current IP address
+	# of the cluster and connect to it.
     cloud_discovery_token="YOUR_CLUSTER_DISCOVERY_TOKEN",
+    # Allow the client to collect metrics
+	# so that you can see client statistics in Management Center.
+	# See https://pkg.go.dev/github.com/hazelcast/hazelcast-go-client#hdr-Management_Center_Integration
     statistics_enabled=True,
+    # Configure TLS
+	# When you download the sample client from the console,
+	# the files are downloaded along with this client code.
     ssl_enabled=True,
     ssl_cafile=abspath("ca.pem"),
     ssl_certfile=abspath("cert.pem"),
     ssl_keyfile=abspath("key.pem"),
     ssl_password="YOUR_SSL_PASSWORD",
 )
+# end::config[]
 
 my_map = client.get_map("map").blocking()
 my_map.put("key", "value")
