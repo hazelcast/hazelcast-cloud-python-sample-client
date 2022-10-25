e4a3e1e39b15296c6a7ae8dc764322f8c55c0283
mdumandag <metin@hazelcast.com>
2022-10-25 17:47:29 +0300

    Update code sample
    This PR updates the code sample according to the unified cloud
    code sample, by backporting the changes merged into the 5.1 branch.
    
    For 4.2, we excluded the SQL related code snippets, just like other
    clients, as the 4.2 server was not capable enough to handle those
    queries.

--- client_with_ssl.py
+++ client_with_ssl.py
@@ -1,15 +1,57 @@
-import hazelcast
 import logging
 import random
+import os
+
+import hazelcast
+from hazelcast.core import HazelcastJsonValue
 from hazelcast.discovery import HazelcastCloudDiscovery
-from os.path import abspath
 
 """
 This is boilerplate application that configures client to connect Hazelcast Cloud cluster.
-After successful connection, it puts random entries into the map.
 
-See: https://docs.hazelcast.cloud/docs/python-client
+See: https://docs.hazelcast.com/cloud/python-client
 """
+
+
+def city(country: str, name: str, population: int) -> HazelcastJsonValue:
+    return HazelcastJsonValue({
+        "country": country,
+        "city": name,
+        "population": population,
+    })
+
+
+def map_example(client: hazelcast.HazelcastClient):
+    """
+    This example shows how to work with Hazelcast maps.
+    """
+    cities = client.get_map("cities").blocking()
+    cities.put("1", city("United Kingdom", "London", 9_540_576))
+    cities.put("2", city("United Kingdom", "Manchester", 2_770_434))
+    cities.put("3", city("United States", "New York", 19_223_191))
+    cities.put("4", city("United States", "Los Angeles", 3_985_520))
+    cities.put("5", city("Turkey", "Ankara", 5_309_690))
+    cities.put("6", city("Turkey", "Istanbul", 15_636_243))
+    cities.put("7", city("Brazil", "Sao Paulo", 22_429_800))
+    cities.put("8", city("Brazil", "Rio de Janeiro", 13_634_274))
+    map_size = cities.size()
+    print(f"'cities' map now contains {map_size} entries.")
+    print("-"*20)
+
+
+def nonstop_map_example(client: hazelcast.HazelcastClient):
+    print("Now the map named 'map' will be filled with random entries.")
+    map = client.get_map("map").blocking()
+    iteration_counter = 0
+    while True:
+        random_key = random.randint(0, 99_999)
+        map.put(f"key-{random_key}", f"value-{random_key}")
+        map.get(f"key-{random.randint(0, 99_999)}")
+        iteration_counter += 1
+        if iteration_counter == 10:
+            iteration_counter = 0
+            print(f"Current map size: {map.size()}")
+
 
 logging.basicConfig(level=logging.INFO)
 HazelcastCloudDiscovery._CLOUD_URL_BASE = "YOUR_DISCOVERY_URL"
@@ -18,28 +60,13 @@ client = hazelcast.HazelcastClient(
     cloud_discovery_token="YOUR_CLUSTER_DISCOVERY_TOKEN",
     statistics_enabled=True,
     ssl_enabled=True,
-    ssl_cafile=abspath("ca.pem"),
-    ssl_certfile=abspath("cert.pem"),
-    ssl_keyfile=abspath("key.pem"),
+    ssl_cafile=os.path.abspath("ca.pem"),
+    ssl_certfile=os.path.abspath("cert.pem"),
+    ssl_keyfile=os.path.abspath("key.pem"),
     ssl_password="YOUR_SSL_PASSWORD",
 )
+print("Connection Successful!")
 
-my_map = client.get_map("map").blocking()
-my_map.put("key", "value")
-
-if my_map.get("key") == "value":
-    print("Successfully connected!")
-    print("Now, 'map' will be filled with random entries.")
-
-    iteration_counter = 0
-    while True:
-        random_key = str(random.randint(1, 100000))
-        my_map.put("key" + random_key, "value" + random_key)
-        my_map.get("key" + random_key)
-        iteration_counter += 1
-        if iteration_counter == 10:
-            iteration_counter = 0
-            print("Map size:", my_map.size())
-else:
-    client.shutdown()
-    raise Exception("Connection failed, check your configuration.")
+map_example(client)
+# nonstop_map_example(client)
+client.shutdown()
